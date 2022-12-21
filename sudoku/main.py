086ad52d24ec4dd7393b9c54ae0cf6379d558ed1
josiah <josiah123t@gmail.com>
2022-12-22 00:32:52 +1100

    simplifications

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,59 +1,64 @@
-from numpy import array
+import itertools
+import numpy as np
+
+matrix = np.array([
+    [7, 0, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 2],
+    [0, 0, 0, 2, 0, 4, 0, 3, 0],
+    [0, 0, 7, 0, 0, 6, 0, 8, 9],
+    [0, 0, 3, 7, 0, 5, 4, 0, 0],
+    [8, 1, 0, 4, 0, 0, 2, 0, 0],
+    [0, 5, 0, 6, 0, 1, 0, 0, 0],
+    [4, 0, 0, 0, 0, 0, 6, 0, 0],
+    [0, 0, 0, 0, 0, 0, 9, 0, 3],
+    ])
+
+height = matrix.shape[0]
+width = matrix.shape[1]
+
+mini_height = int(height ** 0.5)
+mini_width = int(width ** 0.5)
 
 
-matrix = (
-array([[0, 4, 0, 0, 2, 0, 3, 0, 0],
-       [0, 3, 0, 0, 0, 9, 0, 7, 5],
-       [2, 0, 0, 3, 0, 7, 0, 0, 0],
-       [0, 2, 5, 0, 0, 0, 9, 0, 0],
-       [3, 0, 0, 0, 6, 0, 0, 0, 2],
-       [0, 0, 1, 0, 0, 0, 8, 5, 0],
-       [0, 0, 0, 8, 0, 4, 0, 0, 3],
-       [7, 8, 0, 1, 0, 0, 0, 2, 0],
-       [0, 0, 4, 0, 3, 0, 0, 6, 0]], dtype="object")
-)
 
-length = matrix.shape[0]
-mini_length = int(length ** 0.5)
+def okay(index):
+    current_row = index // width
+    current_column = index % width
+    value = matrix[current_row][current_column]
+    
+    # looping through the row
+    for j in range(width):
+        if j != current_column and matrix[current_row][j] == value:
+            return False;
 
+    # looping through the column
+    for i in range(height):
+        if i != current_row and matrix[i][current_column] == value:
+            return False;
+   
+    # looping through the mini matrix:
 
+    mini_row = (current_row // mini_height) * mini_height
+    mini_column = (current_column // mini_width) * mini_width
 
-def good(current):
-    row_number = current // length
-    column_number = current % length
-    value = matrix[row_number][column_number]
-    for i in range(length):
-        if (matrix[row_number][i] == value and i != column_number) or (matrix[i][column_number] == value and i != row_number):
-            return False
-
-    mini_y = row_number // (length // mini_length)
-    mini_x = column_number // (length // mini_length)
-
-    for i in range(length):
-        mini_row_number = mini_y * (length // mini_length) + i // mini_length
-        mini_column_number = mini_x * (length // mini_length) + i % mini_length
-        
-        if (matrix[mini_row_number][mini_column_number] == value and mini_row_number * length + mini_column_number != current):
-            return False
+    for (x, y) in itertools.product(range(mini_width), range(mini_height)):
+        i = mini_row + y
+        j = mini_column + x
+        if (i, j) != (current_row, current_column) and matrix[i][j] == value:
+            return False;
     return True
 
-    
+def solve(index):
+    print(f"matrix = \n{matrix}")
+    if index == height * width:
+        return False
+    if matrix[index // width][index % width] != 0:
+        return solve(index+1)
+    for i in range(1, height + 1):
+        matrix[index // width][index % width] = i
+        if okay(index) and not solve(index + 1):
+            return False
+    matrix[index // width][index % width] = 0
+    return True;
 
-def solve(current=0):
-    if current == length ** 2:
-        return 0;
-    
-    elif matrix[current // length][current % length]:
-        return solve(current + 1)
-
-    for j in range(1, length + 1):
-        matrix[current // length][current % length] = j
-        # print(f"matrix = \n{matrix}")
-        if (good(current) and not solve(current + 1)):
-            return 0;
-            
-    matrix[current // length][current % length] = 0
-    return 1;
-
-print("impossible" if solve() else "done")
-print(matrix)
+print("impossible" if solve(0) else matrix)
