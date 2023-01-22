4b7d6e58c34ca87bc0cdeffadf09b46ee7f8a2d4
josiah <josiah123t@gmail.com>
2023-01-22 16:09:28 +1100

    much better implementation of sparse table with overlaps

--- sparse_table/main.cpp
+++ sparse_table/main.cpp
@@ -15,66 +15,77 @@ int log2Floor(unsigned long i){
 	return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
 }
 
+
+
 class SparseTable{
 	private:
 		vector<int> array;
-		int k;
-		int n;
 	protected:
-		int (*function)(int, int);
-		vector<vector<int>> dp; // K = 25, MAX_N = 10 ^ 7
+		int (*function) (int, int);
+		vector<vector<int>> dp;
+		int n;
+		int k;
 	public:
-		SparseTable(vector<int> array, int (*function)(int, int)): array(array), function(function){
-			// O(N log(N)) (space and time)
+		SparseTable(vector<int> &array, int (*function) (int, int)): array(array), function(function){
 			n = array.size();
 			k = log2Floor(n);
-			dp = vector<vector<int>> (k, vector<int>(n));
-			exit(0);
+			dp = vector<vector<int>>(k + 1, vector<int>(n));
+			// dp[i][j] = [j, (j + 2^i - 1)]
 			std::copy(array.begin(), array.end(), dp[0].begin());
-
 			for (int i = 1; i <= k; i++){
-				for (int j = 0; j + (1 << i) <= n; j++){
-					// [j, j + 2 ^ i - 1] = [j, j + 2 ^ (i - 1) - 1] + [j + 2 ^ (i - 1), j + 2 ^ i - 1];
+				for (int j = 0; (j + (1 << i)) <= n; j++){
+					// [j, (j + 2 ^ i - 1)] = [j, (j + 2 ^ (i - 1) - 1)] + [j + 2 ^ (i - 1), (j + 2 ^ i - 1)]
 					dp[i][j] = function(dp[i - 1][j], dp[i - 1][j + (1 << (i - 1))]);
-					cout << "i: " << i << ", j: " << j << ", state[i][j]: " << dp[i][j] << endl;
 				}
 			}
 		}
-		long long query(int left, int right){
-			// O (log(N));
-			long long result = 0;
+		int query(int left, int right) {
+			int answer = 0;
 			for (int i = k; ~i; i--){
-				if ((1 << i) <= right - left + 1){
-					result = function(result, dp[i][left]);
+				if (1 << i <= right - left + 1){
+					answer = function(answer, dp[i][left]);
 					left += 1 << i;
 				}
 			}
-			return result;
+			return answer;
 		}
 };
 
-
-class SparseTableMax : public SparseTable {
+class TwoOverlapSparseTable : public SparseTable {
 	public:
-		SparseTableMax(vector<int> array) : SparseTable(array, [](int a, int b){return a > b ? a : b;}){
-			// O(N log(N)) (space and time)
-		}
+		TwoOverlapSparseTable(vector<int> &array, int(*function)(int, int)) : SparseTable(array, function){}
 		int query(int left, int right){
-			// O(1)
 			int length = right - left + 1;
 			int log_length = log2Floor(length);
-			// function((left, left + 2 ** log_length - 1), (right - 2 ** log_length + 1, right));
-			// dp[i][j] = (j, j + 2 ^ i - 1);
 			return function(dp[log_length][left], dp[log_length][right - (1 << log_length) + 1]);
-	}
+		}
+};
+
+class SparseTableMax : public TwoOverlapSparseTable {
+	public:
+		SparseTableMax(vector<int> &array) : TwoOverlapSparseTable(array, [](int a, int b){return a > b ? a : b;}){
+		}
+};
+
+class SparseTableMin : public TwoOverlapSparseTable {
+	public:
+		SparseTableMin(vector<int> &array) : TwoOverlapSparseTable(array, [](int a, int b){return a < b ? a : b;}){
+		}
 };
 
 int main(void){
-	SparseTable sparse_table_sum = SparseTable({1, 2, 3, 4}, [](int a, int b){return a + b;});
+	vector<int> array;
+	array = {1, 2, 3, 4};
+	SparseTable sparse_table_sum = SparseTable(array, [](int a, int b){return a + b;});
 	assert(sparse_table_sum.query(2, 3) == 7);
 	assert(sparse_table_sum.query(1, 3) == 9);
-	SparseTableMax sparse_table_max = SparseTableMax({1, 2, 3, 4});
+	array = {1, 2, 3, 4};
+	SparseTableMax sparse_table_max = SparseTableMax(array);
 	assert(sparse_table_max.query(2, 3) == 4);
 	assert(sparse_table_max.query(1, 3) == 4);
 	assert(sparse_table_max.query(1, 2) == 3);
+	SparseTableMin sparse_table_min = SparseTableMin(array);
+	assert(sparse_table_min.query(2, 3) == 3);
+	assert(sparse_table_min.query(1, 3) == 2);
+	assert(sparse_table_min.query(1, 2) == 2);
 }
